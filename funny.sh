9eb4c70f569b3ba8294c5ef2185d996e7a507b67
trangtrang66 <52643374+trangtrang66@users.noreply.github.com>
2021-03-19 10:58:42 +1100

    Update funny.sh

--- funny.sh
+++ funny.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #save as funny.sh
 if [[ $# -eq 0 ]] ; then
-    echo "This is NOT funny"
-    exit 
+	echo "This is NOT funny"
+	exit 
 else 
-		echo "This is funny"
+	echo "This is funny"
 fi
